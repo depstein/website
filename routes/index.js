b9b6429bb745ea5d718bfc37e1992daeb94eba21
Daniel Epstein <depstein@cs.washington.edu>
2016-01-03 14:12:46 -0800

    caching API data

--- routes/index.js
+++ routes/index.js
@@ -17,14 +17,15 @@ var fitbit = require('simple-oauth2')({site: 'https://api.fitbit.com', tokenPath
 var fitbit_access_token = fitbit.accessToken.create({access_token: fitbit_credentials.access_token, refresh_token: fitbit_credentials.refresh_token, expires_in:3600});
 var twitter_client = new twitter(twitter_credentials);
 
+var api_data = {};
+var api_update = null;
+
 var publications = JSON.parse(fs.readFileSync('data/publications.json', 'utf8'));
 var BIB_FILES = 'public/bibtex';
 var bib_data = fs.readdirSync(BIB_FILES).map(function(f) { return formatBib(f, bibtex(fs.readFileSync(BIB_FILES + '/' + f, 'utf-8'))); });
 bib_data.sort(compareBib);
 
 var recentPublications = bib_data.filter(function(f) { return moment(f.date, "MMM YYYY").add(13, 'month').valueOf() >= moment().valueOf() && f.local && (f.type == 'paper' || f.type == 'note');});
-
-//console.log(bib_data);
 
 function formatBib(f, bib) {
 	var name = Object.keys(bib)[0];
@@ -63,28 +64,35 @@ function parseAPICalls(results) {
 	data.kindle = nodes[0].textContent.split("by")[0].trim();
 	data.fitbit = results[1].summary.steps;
 	data.twitter = results[2][0].text;
+	api_data = data;
+	api_update = moment();
 	return data;
 }
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	Promise.all(getPromises()).then(function(results) {
-		res.render('index', extend(parseAPICalls(results), {'bib':recentPublications}));
-	}).catch(function(err) { //try refreshing the fitbit token to see if that helps...
-		console.log('Refreshing fitbit token...');
-		fitbit_access_token.refresh().then(function saveToken(newToken) {
-			fitbit_access_token = newToken;
-			//write the new token to the credentials file, in hope this saves us in the future
-			fitbit_credentials.access_token = newToken.token.access_token;
-			fitbit_credentials.refresh_token = newToken.token.refresh_token;
-			fs.writeFileSync('data/fitbit_credentials.json', JSON.stringify(fitbit_credentials));
-			Promise.all(getPromises()).then(function(results) { //try again
-				res.render('index', extend(parseAPICalls(results), {'bib':recentPublications}));
-			}).catch(function(err) {
-				res.render('index', {kindle:'Nothing', fitbit:0, twitter:'Nothing', 'bib':recentPublications});
-			})
+	if(api_update != null && api_update.diff(moment(), 'hours') < 1) { //API data is recent enough
+		res.render('index', extend(api_data, {'bib':recentPublications}));
+	} else { //retrieve new API data
+		console.log("Getting fresh API data...");
+		Promise.all(getPromises()).then(function(results) {
+			res.render('index', extend(parseAPICalls(results), {'bib':recentPublications}));
+		}).catch(function(err) { //try refreshing the fitbit token to see if that helps...
+			console.log('Refreshing fitbit token...');
+			fitbit_access_token.refresh().then(function saveToken(newToken) {
+				fitbit_access_token = newToken;
+				//write the new token to the credentials file, in hope this saves us in the future
+				fitbit_credentials.access_token = newToken.token.access_token;
+				fitbit_credentials.refresh_token = newToken.token.refresh_token;
+				fs.writeFileSync('data/fitbit_credentials.json', JSON.stringify(fitbit_credentials));
+				Promise.all(getPromises()).then(function(results) { //try again
+					res.render('index', extend(parseAPICalls(results), {'bib':recentPublications}));
+				}).catch(function(err) {
+					res.render('index', {kindle:'Nothing', fitbit:0, twitter:'Nothing', 'bib':recentPublications});
+				})
+			});
 		});
-	});
+	}
 });
 
 router.get('/cv*', function(req, res, next) {
