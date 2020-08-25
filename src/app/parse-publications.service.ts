037b73ba9f5737d6f482186c260d9b0c4f556ea6
Daniel Epstein <epstein@ics.uci.edu>
2020-08-25 14:48:57 -0700

    Refactor publication service
    Sort by order in publications.json rather than an ever-changing set of criteria

--- src/app/parse-publications.service.ts
+++ src/app/parse-publications.service.ts
@@ -8,9 +8,11 @@ import * as moment from 'moment';
   providedIn: 'root'
 })
 export class ParsePublicationsService {
+	private publicationList:any[];
 	private allPublications:any[] = [];
 	public static readonly ARCHIVAL:string[] = ['paper', 'note', 'journal', 'bookchapter'];
 	public static readonly MAPPING:{} = {'paper':['paper', 'note'], 'journal':['journal'], 'workshop':['organizer', 'wip', 'workshop', 'poster', 'panel'], 'book':['dissertation', 'bookchapter']};
+
 
   constructor(private http:HttpClient) {
   }
@@ -18,11 +20,11 @@ export class ParsePublicationsService {
   getPublications():Observable<any[]> {
   	return new Observable((observer) => {
   		this.http.get('./assets/publications.json').subscribe(pubs => {
-	  		let pubList = pubs as {};
-	  		Object.keys(pubList).forEach(pub => {
-	  			let pubURL = './assets/bibtex/' + pub + '.bib';
+  			this.publicationList = pubs as [];
+	  		this.publicationList.forEach(pub => {
+	  			let pubURL = './assets/bibtex/' + pub['key'] + '.bib';
 	  			this.http.get(pubURL, {responseType: 'text'}).subscribe(bib => {
-	  				this.addBib(bibtex.toJSON(bib), pub, pubURL, pubList[pub]);
+	  				this.addBib(bibtex.toJSON(bib), pubURL, pub);
 	  				observer.next(this.allPublications);
 	  			});
 	  		});
@@ -31,9 +33,8 @@ export class ParsePublicationsService {
   	})
   }
 
-  addBib(bib, key, url, labels) {
+  addBib(bib, url, labels) {
   	bib = Object.assign({}, bib[0].entryTags, labels);
-	bib['key'] = key;
 	bib['bib'] = url;
 	bib.author = bib.author.split(" and ").map(function(n) {return n.split(",").reverse().join(' ').replace(/ /g, "\xa0").trim()}); //reverse hack will work in all trivial cases, e.g. Epstein, Daniel A.
 	if('booktitle' in bib) { //Most of my publications have a book title field (e.g., conference publications).
@@ -50,32 +51,9 @@ export class ParsePublicationsService {
 		bib.series = bib.series.replace(/'/g, 20); // '15 -> 2015
 	}
 	this.allPublications.push(bib);
-	this.allPublications.sort(this.compareBib);
-}
-
-compareBib(a, b) {
-	var ma = moment(a.date, "MMM YYYY");
-	var mb = moment(b.date, "MMM YYYY");
-	if(ma != mb) {
-		return ma > mb ? -1 : 1;
-	}
-	var typeSortOrder = ['journal', 'paper', 'note', 'organizer', 'wip', 'workshop', 'poster', 'dissertation'];
-	var ta = typeSortOrder.indexOf(a.type);
-	var tb = typeSortOrder.indexOf(b.type)
-	if(ta != tb) {
-		return ta > tb ? 1 : -1;
-	}
-	//There are non-breaking spaces in these strings. Be careful of that when copy-pasting.
-	var oa = a.author.indexOf('Daniel A. Epstein');
-	var ob = b.author.indexOf('Daniel A. Epstein');
-	if(oa != ob) {
-		return oa > ob ? 1 : -1;
-	}
-	var al = a.author.length;
-	var bl = b.author.length;
-	if(al != bl) {
-		return al > bl ? 1 : -1;
-	}
-	return 0; //TODO: more detailed sorting when pubs were published at the same time at the same "priority" level
-}
+	//TODO: This operation is not a problem as long as I have few publications, but could start taking longer...
+	this.allPublications.sort((a, b) => {
+		return this.publicationList.findIndex(f => f.key == a.key) < this.publicationList.findIndex(f => f.key == b.key) ? -1 : 1;
+	});
+  }
 }
