f38a12d5f52817d733d164db3b0c78e553c5ae15
Daniel Epstein <epstein@ics.uci.edu>
2020-08-26 15:45:24 -0700

    Further refactor get publications

--- src/app/parse-publications.service.ts
+++ src/app/parse-publications.service.ts
@@ -8,8 +8,7 @@ import * as moment from 'moment';
   providedIn: 'root'
 })
 export class ParsePublicationsService {
-	private publicationList:any[];
-	private allPublications:any[] = [];
+	private publicationList:any[] = [];
 	public static readonly ARCHIVAL:string[] = ['paper', 'note', 'journal', 'bookchapter'];
 	public static readonly MAPPING:{} = {'paper':['paper', 'note'], 'journal':['journal'], 'workshop':['organizer', 'wip', 'workshop', 'poster', 'panel'], 'book':['dissertation', 'bookchapter']};
 
@@ -19,41 +18,44 @@ export class ParsePublicationsService {
 
   getPublications():Observable<any[]> {
   	return new Observable((observer) => {
-  		this.http.get('./assets/publications.json').subscribe(pubs => {
-  			this.publicationList = pubs as [];
-	  		this.publicationList.forEach(pub => {
-	  			let pubURL = './assets/bibtex/' + pub['key'] + '.bib';
-	  			this.http.get(pubURL, {responseType: 'text'}).subscribe(bib => {
-	  				this.addBib(bibtex.toJSON(bib), pubURL, pub);
-	  				observer.next(this.allPublications);
+  		this.http.get('./assets/publications.json').subscribe((pubs:any[]) => {
+  			let pubCount = 0;
+  			pubs.forEach(pub => {
+  				let entry = {};
+  				//Add the entry, the fields will be updated by the asynchronous bibtex calls.
+  				this.publicationList.push(entry);
+  				observer.next(this.publicationList);
+  				let pubURL = './assets/bibtex/' + pub['key'] + '.bib';
+  				this.http.get(pubURL, {responseType: 'text'}).subscribe((bib:any) => {
+	  				bib = bibtex.toJSON(bib);
+	  				bib = Object.assign({}, bib[0]['entryTags'], pub);
+					bib['bib'] = pubURL;
+					bib['author'] = bib['author'].split(" and ").map(function(n) {return n.split(",").reverse().join(' ').replace(/ /g, "\xa0").trim()}); //reverse hack will work in all trivial cases, e.g. Epstein, Daniel A.
+					if('booktitle' in bib) { //Most of my publications have a book title field (e.g., conference publications).
+						bib['booktitle'] = bib['booktitle'].replace(/\\/g, "").replace(/#38;/g, "");
+						//Put organizer roles in parantheses, because the series often includes "adjunct" or "extended abstracts"
+						if(bib['booktitle'].includes('Organizer')) {
+							bib['booktitle'] = '(' + bib['booktitle'] + ')';
+						}
+					}
+					if('journal' in bib) { //A few have journals instead.
+						bib['journal'] = bib['journal'].replace(/\\/g, "").replace(/#38;/g, "");
+					}
+					if('series' in bib) {
+						bib['series'] = bib['series'].replace(/'/g, 20); // '15 -> 2015
+					}
+					Object.keys(bib).forEach(b => { //shallow copy into the entry and notify the observable
+						entry[b] = bib[b];
+					});
+					observer.next(this.publicationList);
+					pubCount++;
+					if(pubCount == pubs.length) {
+						observer.complete();
+					}
 	  			});
-	  		});
+  			});
 	  	});
   		return {unsubscribe() {}};
   	})
-  }
-
-  addBib(bib, url, labels) {
-  	bib = Object.assign({}, bib[0].entryTags, labels);
-	bib['bib'] = url;
-	bib.author = bib.author.split(" and ").map(function(n) {return n.split(",").reverse().join(' ').replace(/ /g, "\xa0").trim()}); //reverse hack will work in all trivial cases, e.g. Epstein, Daniel A.
-	if('booktitle' in bib) { //Most of my publications have a book title field (e.g., conference publications).
-		bib.booktitle = bib.booktitle.replace(/\\/g, "").replace(/#38;/g, "");
-		//Put organizer roles in parantheses, because the series often includes "adjunct" or "extended abstracts"
-		if(bib.booktitle.includes('Organizer')) {
-			bib.booktitle = '(' + bib.booktitle + ')';
-		}
-	}
-	if('journal' in bib) { //A few have journals instead.
-		bib.journal = bib.journal.replace(/\\/g, "").replace(/#38;/g, "");
-	}
-	if('series' in bib) {
-		bib.series = bib.series.replace(/'/g, 20); // '15 -> 2015
-	}
-	this.allPublications.push(bib);
-	//TODO: This operation is not a problem as long as I have few publications, but could start taking longer...
-	this.allPublications.sort((a, b) => {
-		return this.publicationList.findIndex(f => f.key == a.key) < this.publicationList.findIndex(f => f.key == b.key) ? -1 : 1;
-	});
   }
 }
