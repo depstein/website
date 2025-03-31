a43b6405ee1c8eafa4832ef7f9a3f0f34bf6623c
Daniel Epstein <epstein@ics.uci.edu>
2025-03-31 10:22:51 -0700

    Add projects and CHI honorable mention

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -26,19 +26,19 @@ export class ProjectsComponent implements OnInit {
 
   filterPublications() {
   	this.practicesPublications = this.allPublications.filter((pub:{}) => {
-  		return ['cscw24a', 'chi24d', 'chi23a', 'cscw22b', 'chi22a', 'cscw21a','jmh21','dis21b','chi21a', 'chi21b', 'cscw20b', 'chi17a', 'chi16c', 'ubi15', 'chi15'].includes(pub['key']);
+  		return ['chi25c', 'cscw24a', 'chi24d', 'chi23a', 'cscw22b', 'chi22a', 'cscw21a','jmh21','dis21b','chi21a', 'chi21b', 'cscw20b', 'chi17a', 'chi16c', 'ubi15', 'chi15'].includes(pub['key']);
   	});
 
   	this.designPublications = this.allPublications.filter((pub:{}) => {
-  		return ['chi24a', 'imwut24', 'mobilehci23', 'imwut22', 'imwut21','dis21a', 'ubi16', 'chi16a', 'dis14', 'pervasivehealth20'].includes(pub['key']);
+  		return ['chi25a', 'chi24a', 'imwut24', 'mobilehci23', 'imwut22', 'imwut21','dis21a', 'ubi16', 'chi16a', 'dis14', 'pervasivehealth20'].includes(pub['key']);
   	});
 
   	this.socialPublications = this.allPublications.filter((pub:{}) => {
-  		return ['cscw24c', 'chi23c', 'cscw22d', 'cscw21c', 'cscw20a', 'gi20', 'cscw17_online', 'chi16b', 'cscw15', 'ubi13'].includes(pub['key']);
+  		return ['cscw25', 'chi25d', 'cscw24c', 'chi23c', 'cscw22d', 'cscw21c', 'cscw20a', 'gi20', 'cscw17_online', 'chi16b', 'cscw15', 'ubi13'].includes(pub['key']);
   	});
 
     this.clinicalPublications = this.allPublications.filter((pub:{}) => {
-      return ['cscw24b', 'chi24c', 'jmh23', 'chi23b','chi22b', 'cscw22a', 'cscw21b', 'dis18', 'chi17b'].includes(pub['key']);
+      return ['chi25b', 'cscw24b', 'chi24c', 'jmh23', 'chi23b','chi22b', 'cscw22a', 'cscw21b', 'dis18', 'chi17b'].includes(pub['key']);
     });
 
     this.reflectingPublications = this.allPublications.filter((pub:{}) => {
